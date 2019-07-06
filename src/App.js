2e7f04899118fa10d04b5f0c6a0fca4fcaa93288
eshward95 <eshward95@gmail.com>
2019-07-06 18:31:07 +0530

    Google analytics

--- src/App.js
+++ src/App.js
@@ -7,9 +7,19 @@ import Details from './components/Details';
 import ProductList from './components/ProductList';
 import Cart from './components/Cart';
 import Default from './components/Default';
+import ReactGA from 'react-ga';
+
 import Modal from './components/Modal'
 function App() {
+  const trackingId = "UA-143393466-1"; // Replace with your Google Analytics tracking ID
+ReactGA.initialize(trackingId);
+// ReactGA.set({
+//   userId: auth.currentUserId(),
+//   // any data that is relevant to the user session
+//   // that you would like to track with google analytics
+// })
   return (
+
  <React.Fragment>
   <Navbar/>
   <Switch>
--- src/index.js
+++ src/index.js
@@ -4,11 +4,20 @@ import './index.css';
 import App from './App';
 import {BrowserRouter as Router}from 'react-router-dom';
 import {ProductProvider} from './context'
+import ReactGA from 'react-ga';
+import { createBrowserHistory } from 'history';
 import * as serviceWorker from './serviceWorker';
 
+const history = createBrowserHistory();
+
+// Initialize google analytics page view tracking
+history.listen(location => {
+  ReactGA.set({ page: location.pathname }); // Update the user's current page
+  ReactGA.pageview(location.pathname); // Record a pageview for the given page
+});
 ReactDOM.render(
     <ProductProvider>
-        <Router>
+        <Router history={history}>
             <App />
         </Router>
     </ProductProvider>, 
